e96f0fc89748b6017a3a46d74136a9cfec487a41
Trim Bresilla <trim.bresilla@gmail.com>
2025-05-20 03:10:58 +0200

    feat: add GeoJSON processing to the WUR executable
    - Update alias for the executable in .envrc
    - Add compiler warning suppression to CMakeLists.txt
    - Add new GeoJSON file for Wageningen University and Research location
    - Add a new executable run script
    - Read and process GeoJSON data in the executable
    - Use updated input file and print feature count and geometry type

--- src/exe.cpp
+++ src/exe.cpp
@@ -1,11 +1,23 @@
 #include "concord/types_basic.hpp"
 #include "geoson/parser.hpp"
 
+#include <iostream>
+
 int main() {
 
     concord::Datum datum{/*lat=*/52.0, /*lon=*/4.4, /*alt=*/0.0};
 
-    auto fc = geoson::ReadFeatureCollection("../misc/field4.geojson", datum);
+    auto fc = geoson::ReadFeatureCollection("misc/wur.geojson", datum);
+
+    std::cout << fc.features.size() << " features\n" << std::endl;
+
+    for (auto const &f : fc.features) {
+        std::cout << "  properties: " << f.properties.size() << "\n";
+        auto &v = f.geometry;
+        if (auto *p = std::get_if<concord::Polygon>(&v)) {
+            std::cout << "    Polygon\n";
+        }
+    }
 
     return 0;
 }
